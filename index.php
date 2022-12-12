fe8b5471bef09790e9378f37eaad57204cf27958
talitha <89697381+talithasyafiyah@users.noreply.github.com>
2022-12-12 13:49:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap"
+    />
+</head>
+<body style="background: url(space.jpg); background-repeat: none; background-size: cover;">
+    <div class="row text-center">
+        <div class="col-12 mt-5"><p>dsfsd</p></div>
+    </div>
+    <div class="row">
+        <div class="col-3"><p>dsfd</p></div>
+        <div class="col-6 text-center">
+            <div class="text"><p>Eevee</p></div>
+        </div>
+        <div class="col-3">dff</div>
+    </div>
+    <div class="row">
+        <div class="col-6">dkafsmd</div>
+        <div class="col-6">.ldksf</div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</body>
+</html>
